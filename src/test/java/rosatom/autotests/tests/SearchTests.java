a442f7d131623cc54947c65d33c0ccb91de8199b
Elena <58141818+ElenaSkorobodilova@users.noreply.github.com>
2022-01-08 16:21:49 +0700

    update

--- src/test/java/rosatom/autotests/tests/SearchTests.java
+++ src/test/java/rosatom/autotests/tests/SearchTests.java
@@ -22,6 +22,7 @@ public class SearchTests extends TestBase {
     @DisplayName("Поиск конкретной вакансии")
     void vacancySearchTest() {
         step(String.format("Вводим в строке поиска вакансию '%s'", searchVacancy), () -> {
+            $("header").scrollIntoView(true);
             $("input.chakra-input.css-nwb3dv").click();
             $("input.chakra-input.css-nwb3dv").sendKeys(searchVacancy);
             $("button.chakra-button.css-3diha").click();
